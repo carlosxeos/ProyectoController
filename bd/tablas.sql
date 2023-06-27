58ae91936c43d74ceaf94b9bb16e2f8533751677
LuisLucioDoTech1998 <75852335+LuisLucioDoTech1998@users.noreply.github.com>
2023-06-27 00:18:44 -0600

    Se agregan tablas faltantes

--- bd/tablas.sql
+++ bd/tablas.sql
@@ -2,17 +2,6 @@ create database DotechProyectSystem
 
 use DotechProyectSystem
 
-create table tbUsuario (
-    idUsuario int identity (1,1) primary key ,
-    idTipoUsuario int not null,
-    userName nvarchar(50) not null,
-    password nvarchar(MAX) not null ,
-    nombreCompleto nvarchar(100) not null,
-    fechaCreacion datetime not null ,
-    fechaModificacion datetime,
-    metadata nvarchar(1000)
-)
-
 create table ctTipoUsuario(
     idTipoUsuario int identity (1,1) primary key,
     descripcion nvarchar(100) not null,
@@ -20,6 +9,18 @@ create table ctTipoUsuario(
     accionesPorton varchar(1) not null,
     accionesClima varchar(1) not null,
     comentarios nvarchar(500)
+)
+
+create table ctUsuario (
+    idUsuario int identity (1,1) primary key ,
+    idTipoUsuario int not null,
+    userName nvarchar(50) not null,
+    password nvarchar(MAX) not null ,
+    nombreCompleto nvarchar(100) not null,
+    fechaCreacion datetime not null ,
+    fechaModificacion datetime,
+    metadata nvarchar(1000),
+    foreign key (idTipoUsuario) references ctTipoUsuario(idTipoUsuario),
 )
 
 create table ctTipoMovimiento(
@@ -34,7 +35,7 @@ create table tbHistorial(
     fecha datetime not null,
     idTipoMovimiento int,
     valor nvarchar(100),
-    foreign key(idUsuario) references tbUsuario(idUsuario),
+    foreign key(idUsuario) references ctUsuario(idUsuario),
     foreign key(idTipoMovimiento) references ctTipoMovimiento(idTipoMovimiento)
 )
 
@@ -42,18 +43,30 @@ create table tbClima(
     idClima int identity (1,1) primary key,
     temperatura decimal(5,3),
     ultModificacion datetime,
-    idUsuario int foreign key references tbUsuario(idUsuario),
+    idUsuario int foreign key references ctUsuario(idUsuario),
     idTipoModificacion int not null,
     descripcion nvarchar(200),
-    idEmpresa int not null
+    idEmpresa int not null,
+    temperaturaMinDeseada decimal not null,
+    temperaturaMaximaDeseada decimal not null
 )
 
 create table tbPorton(
     idPorton int identity (1,1) primary key,
     estatus varchar(1) not null,
     ultModificacion datetime,
-    idUsuario int foreign key references tbUsuario(idUsuario),
+    idUsuario int foreign key references ctUsuario(idUsuario),
     idTipoModificacion int not null,
     descripcion nvarchar(200),
-    idEmpresa int not null
+    idEmpresa int not null,
+    horarioMinAccion time,
+    horarioMaxAccion time
+)
+
+create table tbContactos (
+    id int  identity (1,1) primary key,
+    telefono nvarchar(20) not null,
+    accionesPorton char(1),
+    accionesClima char(1),
+    nombre nvarchar(100)
 )
